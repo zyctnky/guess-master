5d0ff16aabc4b9e0d01a3eaa10fac263ba615c41
Ziya <ziyacetinkaya@outlook.com>
2023-03-28 16:02:25 +0300

    Revert "Google login entegrated."
    This reverts commit 326aa53044a5a3063bbe2eb60a85bf1a4d0a023c.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Guess Master</title>
-        <meta name="description" content="Play guessing word app" />
+        <meta name="description" content="Play guessing word game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Component {...pageProps} />
     </>
   );
 }
--- src/pages/api/difficulty-levels/index.ts
+++ src/pages/api/difficulty-levels/index.ts
@@ -2,8 +2,6 @@ import path from "path";
 import { promises as fs } from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { DifficultyLevel } from "@/interfaces/interfaces";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
 
 type Data = {
   data: Object;
@@ -13,20 +11,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const session = await getServerSession(req, res, authOptions);
+  const jsonDirectory = path.join(process.cwd(), "json");
+  const fileContents = await fs.readFile(
+    jsonDirectory + "/difficulty-levels.json",
+    "utf8"
+  );
+  const difficultLevels: DifficultyLevel[] = JSON.parse(fileContents);
 
-  if (session) {
-    const jsonDirectory = path.join(process.cwd(), "json");
-    const fileContents = await fs.readFile(
-      jsonDirectory + "/difficulty-levels.json",
-      "utf8"
-    );
-    const difficultLevels: DifficultyLevel[] = JSON.parse(fileContents);
-
-    res.status(200).json({
-      data: difficultLevels,
-    });
-  } else {
-    res.status(401).json({ data: "Unauthorized" });
-  }
+  res.status(200).json({
+    data: difficultLevels,
+  });
 }
